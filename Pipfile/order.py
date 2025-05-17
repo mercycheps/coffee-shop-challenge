903200aacba6514bed1ecadb7ab4411d85cbdc6f
Mercy <maritimmercy8@gmail.com>
2025-05-18 00:37:29 +0300

    created order class,its condition

--- Pipfile/order.py
+++ Pipfile/order.py
@@ -1,15 +1,16 @@
-class customer:
- def __init__(self, name): 
-  self.name = name
-  self._orders = []
-  
-  @property
-  def name(self):
-      return self._name
-  
-  @name.setter
-  def name(self, value):
-    if isinstance(value, str) and 1<= len(value) <= 15:
-          self._name = value
-    else:
-        raise Exception("Name must be a string between 1 and 15 characters")
+class order:
+ def __init__(self, customer, coffee, price):
+     self._customer = customer
+     self._coffee = coffee
+     self._price = float(price)
+     
+     if not isinstance(customer, customer):
+         raise TypeError("customer must be a Customer instance")
+     
+     if not isinstance(coffee, coffee):
+         raise TypeError("coffee must be a Coffee instance")
+     if not(isinstance(price, float) or isinstance(price, int)) or not (1.0 <= price <= 10.0):
+         raise Exception("Price must be a float between 1.0 and 10.0")
+     
+     
+        
