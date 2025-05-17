3753591343312419ee54fe7b9570bb2bc8f6233e
Mercy <maritimmercy8@gmail.com>
2025-05-18 00:10:10 +0300

    added class, property

---
+++ Pipfile/customer.py
@@ -0,0 +1,18 @@
+class customer:
+    
+def __init__(self, name): 
+ self.name = name
+ self._orders = []
+  
+@property
+def name(self):
+      return self._name
+  
+  
+@name.setter
+  
+def name(self, value):
+    if isinstance(value, str) and 1<= len(value) <= 15:
+          self._name = value
+    else:
+        raise Exception("Name must be a string between 1 and 15 characters")
